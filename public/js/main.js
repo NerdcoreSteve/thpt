a2f6c11cdbee3e6be04459799b62631d7bc59eec
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-05-06 01:39:28 -0600

    thpt

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,17 @@
-var audio = new Audio('public/audio/thpt.m4a')
-audio.play()
+var audio = new Audio('public/audio/thpt.mp3')
+
+var face = document.querySelector('.face')
+
+var poop = false
+
+face.onclick =
+    function () {
+        if(!poop) {
+            poop = true
+            face.setAttribute('src', 'public/images/poop_face.png')
+            audio.play()
+        } else {
+            poop = false
+            face.setAttribute('src', 'public/images/some_face.png')
+        }
+    }
