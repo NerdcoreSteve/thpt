7f3d6bb8cf22b594a3eb5b5ed71bd90f800472fd
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-05-05 19:51:23 -0600

    re-added port logic for heroku's benifit

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express')
 var app = express()  
 app.set('view engine', 'pug')
 
+app.set('port', (process.env.PORT || 5000))
+
 app.get('/', function (req, res) {  
-    res.render(
-        'index',
-        { title: 'What?', message: 'Yo Yo'})
+    res.render('index')
 })
 
-app.listen(5000, function () {  
-    console.log('thpt listening on port 5000')
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'))
 })
